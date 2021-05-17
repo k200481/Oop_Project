4df89b911f922ce1683838a37c8f9d31812c8947
Sarim <msaarimkh@gmail.com>
2021-05-17 19:35:50 +0500

    Game error can work with LPCWSTR

--- H_Engine/H_Engine/gameError.h
+++ H_Engine/H_Engine/gameError.h
@@ -16,19 +16,23 @@ class GameError : public std::exception{
 private:
     int     errorCode;
     std::string message;
+    LPCWSTR w_message;
 public:
     
     GameError() throw() :errorCode(gameErrorNS::FATAL_ERROR), message("Undefined Error in game.") {}
     GameError(const GameError& e) throw(): std::exception(e), errorCode(e.errorCode), message(e.message) {}
     GameError(int code, const std::string &s) throw() :errorCode(code), message(s) {}
+    GameError(int code, const LPCWSTR s) throw() : errorCode(code), w_message(s) {}
     GameError& operator= (const GameError& rhs) throw() {
         std::exception::operator=(rhs);
         this->errorCode = rhs.errorCode;
         this->message = rhs.message;
     }
-    virtual ~GameError() throw() {};
+    virtual ~GameError() noexcept {};
     virtual const char* what() const throw() { return this->getMessage(); }
+    virtual LPCWSTR w_what() const throw() { return this->w_getMessage(); }
     const char* getMessage() const throw() { return message.c_str(); }
+    const LPCWSTR w_getMessage() const throw() { return w_message; }
     int getErrorCode() const throw() { return errorCode; }
 };
 
